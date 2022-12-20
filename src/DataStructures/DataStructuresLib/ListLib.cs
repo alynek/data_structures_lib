2d4719696a0b0da53733eae6d07de93f42fc9d23
alynek <alyne0x67@gmail.com>
2022-12-20 00:09:51 -0300

    Create Print method

--- src/DataStructures/DataStructuresLib/ListLib.cs
+++ src/DataStructures/DataStructuresLib/ListLib.cs
@@ -1,7 +1,31 @@
+using System.Text;
+
 namespace DataStructuresLib
 {
-    public class ListLib
+    public class ListLib<T> where T : class
     {
-        
+        private T[] tempList = new T[1];
+        private int total { get; set; } = 0;
+
+        public string Print()
+        {
+            if(total == 0)
+            {
+                return "";
+            }
+
+            StringBuilder builder = new StringBuilder();
+            builder.Append("[");
+
+            for(int i = 0; i < total - 1; i++)
+            {
+                builder.Append(tempList[i]);
+                builder.Append(", ");
+            }
+            
+            builder.Append(tempList[total - 1]);
+            builder.Append("]");
+            return builder.ToString();
+        }
     }
 }
