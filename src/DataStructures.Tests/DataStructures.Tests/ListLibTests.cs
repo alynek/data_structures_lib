4214018673654a745f5213fbb22677cd77dfd5c4
alynek <alyne0x67@gmail.com>
2022-12-20 00:38:58 -0300

    Create test for Add method

--- src/DataStructures.Tests/DataStructures.Tests/ListLibTests.cs
+++ src/DataStructures.Tests/DataStructures.Tests/ListLibTests.cs
@@ -11,4 +11,18 @@ public class ListLibTests
         ListLib<string> listLib = new ListLib<string>();
         Assert.Equal(listLib.Print(), "");
     }
+
+    [Fact]
+    public void ListLib_Add_ShouldReturnCorrectValuesWhenAddingAnItemToTheList()
+    {
+        ListLib<string> listLib = new ListLib<string>();
+
+        listLib.Add("Tyrion");
+        listLib.Add("John");
+
+        var quantityListLib = listLib.Count;
+
+        Assert.Contains("[Tyrion, John]", listLib.Print());
+        Assert.Equal(2, quantityListLib);
+    }
 }
