8b4893f86263c675ce68d47fa11824a0cc273dd6
alynek <alyne0x67@gmail.com>
2022-12-20 23:47:58 -0300

    Remove Print method and create a method to access list index

--- src/DataStructures/DataStructuresLib/ListLib.cs
+++ src/DataStructures/DataStructuresLib/ListLib.cs
@@ -1,12 +1,14 @@
-using System.Text;
-
 namespace DataStructuresLib
 {
     public class ListLib<T> where T : class
     {
         private T[] tempList = new T[1];
         private int _count { get; set; } = 0;
-        public int Count{ get{return _count;} }
+        public int Count { get { return _count; } }
+        public T this[int Count]
+        {
+            get { return tempList[Count]; }
+        }
 
         public void Add(T item)
         {
@@ -15,34 +17,13 @@ namespace DataStructuresLib
             _count += 1;
         }
 
-        public string Print()
-        {
-            if(_count == 0)
-            {
-                return "";
-            }
-
-            StringBuilder builder = new StringBuilder();
-            builder.Append("[");
-
-            for(int i = 0; i < _count - 1; i++)
-            {
-                builder.Append(tempList[i]);
-                builder.Append(", ");
-            }
-            
-            builder.Append(tempList[_count - 1]);
-            builder.Append("]");
-            return builder.ToString();
-        }
-
         private void GuaranteeSpace()
         {
-            if(_count == tempList.Length)
+            if (_count == tempList.Length)
             {
                 T[] copyTempList = new T[tempList.Length + 1];
 
-                for(int i = 0; i < tempList.Length; i++)
+                for (int i = 0; i < tempList.Length; i++)
                 {
                     copyTempList[i] = tempList[i];
                 }
