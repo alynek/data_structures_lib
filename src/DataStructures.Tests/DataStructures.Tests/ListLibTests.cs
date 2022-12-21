6fc784b8c304b1bdcd77f6163ae11aa3e85c9cf6
alynek <alyne0x67@gmail.com>
2022-12-20 23:49:53 -0300

    Refactor tests and create test for GuaranteSpace method

--- src/DataStructures.Tests/DataStructures.Tests/ListLibTests.cs
+++ src/DataStructures.Tests/DataStructures.Tests/ListLibTests.cs
@@ -6,14 +6,14 @@ namespace DataStructures.Tests;
 public class ListLibTests
 {
     [Fact]
-    public void ListLib_Print_ShouldPrintEmptyWhenNothingIsAdded()
+    public void ListLib_ShouldReturnZeroSizeWhenNothingIsAdded()
     {
         ListLib<string> listLib = new ListLib<string>();
-        Assert.Equal(listLib.Print(), "");
+        Assert.True(listLib.Count.Equals(0));
     }
 
     [Fact]
-    public void ListLib_Add_ShouldReturnCorrectValuesWhenAddingAnItemToTheList()
+    public void ListLib_Add_ShouldReturnCorrectValuesWhenAddingAnItem()
     {
         ListLib<string> listLib = new ListLib<string>();
 
@@ -22,7 +22,25 @@ public class ListLibTests
 
         var quantityListLib = listLib.Count;
 
-        Assert.Contains("[Tyrion, John]", listLib.Print());
+        Assert.Equal("Tyrion", listLib[0]);
+        Assert.Equal("John", listLib[1]);
         Assert.Equal(2, quantityListLib);
+    }
+
+    [Fact]
+    public void ListLib_Add_MustGuaranteeSpaceWhenAddingManyItens()
+    {
+        ListLib<string> listLib = new ListLib<string>();
+
+        listLib.Add("Cersei");
+        listLib.Add("Daenerys");
+        listLib.Add("Tyrion");
+        listLib.Add("John");
+
+        var quantityListLib = listLib.Count;
+
+        Assert.Equal("Cersei", listLib[0]);
+        Assert.Equal("John", listLib[3]);
+        Assert.Equal(4, quantityListLib);
     }
 }
