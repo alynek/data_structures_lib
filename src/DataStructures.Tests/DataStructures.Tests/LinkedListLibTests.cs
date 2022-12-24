cb9d5da180c1695d91d135fadcd947bdccce2452
alynek <alyne0x67@gmail.com>
2022-12-24 11:40:51 -0300

    Fix AddFirst test and create tests for AddLast method

--- src/DataStructures.Tests/DataStructures.Tests/LinkedListLibTests.cs
+++ src/DataStructures.Tests/DataStructures.Tests/LinkedListLibTests.cs
@@ -14,6 +14,7 @@ namespace DataStructures.Tests
 
             Assert.True(linkedList.Count == 1);
             Assert.Equal("Debian", linkedList.First.Value);
+            Assert.Equal("Debian", linkedList.Last.Value);
         }
 
         [Fact(DisplayName = "Add first with non-empty list")]
@@ -27,6 +28,45 @@ namespace DataStructures.Tests
             Assert.True(linkedList.Count == 2);
             Assert.Equal("Arch", linkedList.First.Value);
             Assert.Equal("Linux Mint", linkedList.Last.Value);
+        }
+
+        [Fact(DisplayName = "Add Last with non-empty list")]
+        public void LinkedListLib_AddLast_AddAtTheEndWhenTheLinkedListIsNotEmpty()
+        {
+            LinkedListLib<string> linkedList = new LinkedListLib<string>();
+
+            linkedList.AddLast("Fedora");
+            linkedList.AddLast("Slackware");
+
+            Assert.True(linkedList.Count == 2);
+            Assert.Equal("Fedora", linkedList.First.Value);
+            Assert.Equal("Slackware", linkedList.Last.Value);
+        }
+
+        [Fact(DisplayName = "Add Last and Add First with non-empty list")]
+        public void LinkedListLib_AddLast_AddAtTheEndAndTheBeginningWhenTheLinkedListIsNotEmpty()
+        {
+            LinkedListLib<string> linkedList = new LinkedListLib<string>();
+
+            linkedList.AddLast("Fedora");
+            linkedList.AddLast("Slackware");
+            linkedList.AddFirst("Arch");
+
+            Assert.True(linkedList.Count == 3);
+            Assert.Equal("Arch", linkedList.First.Value);
+            Assert.Equal("Slackware", linkedList.Last.Value);
+        }
+
+        [Fact(DisplayName = "Add Last with empty list")]
+        public void LinkedListLib_AddLast_AddAtTheEndWhenTheLinkedListIsEmpty()
+        {
+            LinkedListLib<string> linkedList = new LinkedListLib<string>();
+
+            linkedList.AddLast("Pop!_OS");
+
+            Assert.True(linkedList.Count == 1);
+            Assert.Equal("Pop!_OS", linkedList.First.Value);
+            Assert.Equal("Pop!_OS", linkedList.Last.Value);
         }
     }
 }
