8e95ecca3cc08ab94333a37edb948abd3adb9367
alynek <alyne0x67@gmail.com>
2022-12-24 11:26:11 -0300

    Create test for AddFirst method with non-empty list

--- src/DataStructures.Tests/DataStructures.Tests/LinkedListLibTests.cs
+++ src/DataStructures.Tests/DataStructures.Tests/LinkedListLibTests.cs
@@ -5,8 +5,8 @@ namespace DataStructures.Tests
 {
     public class LinkedListLibTests
     {
-        [Fact]
-        public void Test1()
+        [Fact(DisplayName = "Add first with empty list")]
+        public void LinkedListLib_AddFirst_AddAtBeginningWhenTheLinkedListIsEmpty()
         {
             LinkedListLib<string> linkedList = new LinkedListLib<string>();
 
@@ -14,6 +14,19 @@ namespace DataStructures.Tests
 
             Assert.True(linkedList.Count == 1);
             Assert.Equal("Debian", linkedList.First.Value);
+        }
+
+        [Fact(DisplayName = "Add first with non-empty list")]
+        public void LinkedListLib_AddFirst_AddAtBeginningWhenTheLinkedListIsNotEmpty()
+        {
+            LinkedListLib<string> linkedList = new LinkedListLib<string>();
+
+            linkedList.AddFirst("Linux Mint");
+            linkedList.AddFirst("Arch");
+
+            Assert.True(linkedList.Count == 2);
+            Assert.Equal("Arch", linkedList.First.Value);
+            Assert.Equal("Linux Mint", linkedList.Last.Value);
         }
     }
 }
